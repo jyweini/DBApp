ec4cc9a51f156eb6a1c766af45db89894442682c
jyweini <jyweini@163.com>
2015-07-28 10:24:39 +0800

    输出并Log

--- app/src/main/java/com/example/administrator/db/MainActivity.java
+++ app/src/main/java/com/example/administrator/db/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.administrator.db;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -16,18 +18,27 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         MyDBHelper myDBHelper=new MyDBHelper(this,"data",2);
-        SQLiteDatabase dbWrite=myDBHelper.getWritableDatabase();
-        ContentValues cv=new ContentValues();
-        cv.put("name","zhangsan");
-        cv.put("phone","13588830380");
-        dbWrite.insert("user",null,cv);
-
-        cv=new ContentValues();
-        cv.put("name","lisi");
-        cv.put("phone","13858120073");
-        dbWrite.insert("user",null,cv);
-
-        dbWrite.close();
+//        SQLiteDatabase dbWrite=myDBHelper.getWritableDatabase();
+//        ContentValues cv=new ContentValues();
+//        cv.put("name","zhangsan");
+//        cv.put("phone","13588830380");
+//        dbWrite.insert("user",null,cv);
+//
+//        cv=new ContentValues();
+//        cv.put("name","lisi");
+//        cv.put("phone","13858120073");
+//        dbWrite.insert("user",null,cv);
+//
+//        dbWrite.close();
+        SQLiteDatabase dbreader=myDBHelper.getReadableDatabase();
+        Cursor c=dbreader.query("user",null,null,null,null,null,null);
+        while (c.moveToNext()){
+            String name=c.getString(c.getColumnIndex("name"));
+            String phone=c.getString(c.getColumnIndex("phone"));
+            Log.d("lsx", "name:" + name + "|phone:" + phone);
+        }
+        if(c!=null) c.close();
+        if(dbreader!=null) dbreader.close();
     }
 
 
