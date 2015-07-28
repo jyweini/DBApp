7e2caa0f8d752f0e6467bb5942bb6a7f3f237688
jyweini <jyweini@163.com>
2015-07-28 09:33:21 +0800

    shujuku

---
+++ app/src/main/java/com/example/administrator/db/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.administrator.db;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MyDBHelper myDBHelper=new MyDBHelper(this);
+        SQLiteDatabase dbWrite=myDBHelper.getWritableDatabase();
+        ContentValues cv=new ContentValues();
+        cv.put("name","zhangsan");
+        cv.put("phone","13588830380");
+        dbWrite.insert("user",null,cv);
+
+        cv=new ContentValues();
+        cv.put("name","zhangsan");
+        cv.put("phone","13858120073");
+        dbWrite.insert("user",null,cv);
+
+        dbWrite.close();
+    }
+
+
+}
