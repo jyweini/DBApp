b125f80402734838dae9dd252d8e260dd62fc832
jyweini <jyweini@163.com>
2015-07-29 09:26:39 +0800

    ListViewd显示

--- app/src/main/java/com/example/administrator/db/MainActivity.java
+++ app/src/main/java/com/example/administrator/db/MainActivity.java
@@ -3,15 +3,18 @@ package com.example.administrator.db;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
-
+   private ListView mListView;
+    private SimpleCursorAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +35,8 @@ public class MainActivity extends ActionBarActivity {
 //        dbWrite.close();
         SQLiteDatabase dbreader=myDBHelper.getReadableDatabase();
         Cursor c=dbreader.query("user",null,null,null,null,null,null);
+        mAdapter=new SimpleCursorAdapter(this,R.layout.list_item,c,new String[]{"name"},new int[]{R.id.tv_name});
+        mListView.setAdapter(mAdapter);
         while (c.moveToNext()){
             String name=c.getString(c.getColumnIndex("name"));
             String phone=c.getString(c.getColumnIndex("phone"));
